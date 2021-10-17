502303df5dcac3328d2ea2b7549a02495169d9d3
brkcvlk <brkcvlk15@gmail.com>
2021-10-17 17:08:42 +0300

    Add files via upload

---
+++ HackerRank2.js
@@ -0,0 +1,21 @@
+// Day 1: Arithmetic Operators
+
+// Area :
+        
+let length = 14
+
+let width = 13.5
+
+let area = (length * width)
+
+console.log("Alanı :", area)
+
+// Peremiter :
+
+let uzunluk = 14
+
+let genislik = 13.5
+
+let Peremiter = (2*(uzunluk + genislik))
+
+console.log("Çevresi :", Peremiter)
